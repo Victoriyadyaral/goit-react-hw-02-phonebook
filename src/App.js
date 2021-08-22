88b3d94c67cadac539e39844230c24080316880c
Вікторія Щербанич <dyaral696@gmail.com>
2021-08-22 15:32:49 +0300

    step 3 from hw

--- src/App.js
+++ src/App.js
@@ -2,36 +2,53 @@
 import React, { Component } from 'react';
 
 import Container from './components/container/Container';
-import Form from './components/form/Form'
+import Form from './components/form/Form';
+import Filter from './components/filter/Filter';
+
 
 class App extends Component {
   state = {
-  contacts: []
+     contacts: [
+    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
+    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
+    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
+    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+  ],
+    filter: ''
   }
-
-  // formSubmitHandler = (contact) => {
-  //   console.log(contact)
-  //  };
 
   addContact = (contact) => {
     this.setState(({ contacts }) => ({
       contacts: [contact, ...contacts],
     }));
-
-    console.log(this.state.contacts)
   };
 
   deleteContact = () => { };
+
+  changeFilter = (event) => {
+    this.setState({ filter: event.currentTarget.value });
+  }
+
+  getVisibleContacts = () => {
+    const { filter, contacts } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
   
   render() {
+    const visibleContacts = this.getVisibleContacts();
     
     return (
       <Container>
         <h2>Phonebook</h2>
         <Form onSubmit={this.addContact}/>
         <h2>Contacts</h2>
+        <Filter value={this.state.filter} onChange={this.changeFilter} />
         <ul className="contacts">
-          {this.state.contacts.map(({ name, number, id }) => (
+          {visibleContacts.map(({ name, number, id }) => (
             <li
               key={id}
               className="contacts__item"
